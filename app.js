0b041c70885d54a9d887a90bfb07400b2f32360b
palewing <rendo.030@gmail.com>
2019-11-07 16:09:53 +0700

    skeleton code. good luck

---
+++ app.js
@@ -0,0 +1,21 @@
+if(process.env.NODE_ENV === 'development'){
+    require('dotenv').config();
+}
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const PORT = process.env.PORT || 3000;
+const router = require('./routes')
+
+const app = express();
+
+mongoose.connect(process.env.DB_URI, {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify:false});
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use(cors());
+
+app.use('/', router)
+
+app.listen(PORT, () =>{
+    console.log(`listening on port ${PORT}`);
+});
