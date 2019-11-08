c83a92bd10e2243e06083a63f8b96b5599c83a48
Satyo Wicaksana <satyowicaksana@gmail.com>
2019-11-08 13:05:41 +0700

    get image

--- controllers/image.js
+++ controllers/image.js
@@ -2,9 +2,16 @@ const Image = require('../models/Image')
 
 module.exports = {
   find: (req, res, next) => {
-    Image.find()
+    Image.find().sort({createdAt: 'desc'})
       .then(images => {
         res.status(200).json(images)
+      })
+      .catch(next)
+  },
+  findOne: (req, res, next) => {
+    Image.findById(req.params.id)
+      .then(image => {
+        res.status(200).json(image)
       })
       .catch(next)
   },
--- routes/images.js
+++ routes/images.js
@@ -4,5 +4,6 @@ const upload = require('../middlewares/upload')
 
 router.post('/upload', upload.single('file'), ImageController.upload)
 router.get('/', ImageController.find)
+router.get('/:id', ImageController.findOne)
 
 module.exports = router
