adf5cdb3a24c2c980ef86df2c49b4d48b50284c8
elizabethwidiasri <elizabethwidiasri@gmail.com>
2019-11-08 07:49:19 +0700

    adding server register login

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,12 @@
 const router = require('express').Router();
+const userRouter = require('./user')
+
 
 router.get('/', (req, res)=>{
     res.send('hello')
-    
 })
+
+router.use('/user', userRouter)
+
 
 module.exports = router
